2e4f1f9a706b669701ddeabd8d53e3451baea69b
rodcar <ivanrod12@hotmail.com>
2025-03-15 23:54:35 +0000

    Add stats import to the main module for enhanced dataset statistics functionality

--- openpe/__init__.py
+++ openpe/__init__.py
@@ -5,7 +5,7 @@ from .errors import log_error  # Add this import
 from .dataset import Dataset
 from .webscraper import WebScraper
 from .utils import to_json, from_json
-from .module import get_dataset, get_datasets, expand_datasets, download_dataset, save, load, expand_dataset
+from .module import get_dataset, get_datasets, expand_datasets, download_dataset, save, load, expand_dataset, stats
 
 import os
 import json
